5b22f933d30c2d43a9f7219da3e1fa41c1c8d2b3
Bartosz Miller <miller.bartek@gmail.com>
2023-04-28 00:14:14 +0200

    TESLA: improve Clan comparator

--- src/main/java/pl/ing/tesla/game/model/Clan.java
+++ src/main/java/pl/ing/tesla/game/model/Clan.java
@@ -7,7 +7,7 @@ public record Clan(int numberOfPlayers, int points) implements Comparable<Clan> 
     @Override
     public int compareTo(Clan o) {
         return Comparator.comparingInt(Clan::points)
-                .thenComparingDouble(Clan::numberOfPlayers)
+                .thenComparingInt(Clan::numberOfPlayers)
                 .reversed()
                 .compare(this, o);
     }
