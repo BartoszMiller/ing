ccc7a910046e70d530e1dc0579c9634a342fbd77
Bartosz Miller <miller.bartek@gmail.com>
2023-04-27 23:35:14 +0200

    TESLA: simplify Clan

--- src/main/java/pl/ing/tesla/game/model/Clan.java
+++ src/main/java/pl/ing/tesla/game/model/Clan.java
@@ -4,14 +4,10 @@ import java.util.Comparator;
 
 public record Clan(int numberOfPlayers, int points) implements Comparable<Clan> {
 
-    public double playerAvg() {
-        return (double) points / numberOfPlayers;
-    }
-
     @Override
     public int compareTo(Clan o) {
         return Comparator.comparingInt(Clan::points)
-                .thenComparingDouble(Clan::playerAvg)
+                .thenComparingDouble(Clan::numberOfPlayers)
                 .reversed()
                 .compare(this, o);
     }
