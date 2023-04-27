86b7008d4ff993274d0178fd2add584bab318a40
Bartosz Miller <miller.bartek@gmail.com>
2023-04-27 20:48:35 +0200

    TESLA: test all APIs

--- src/test/java/pl/ing/tesla/AtmControllerTest.java
+++ src/test/java/pl/ing/tesla/AtmControllerTest.java
@@ -1,4 +1,4 @@
-package pl.ing.tesla.atm;
+package pl.ing.tesla;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -8,14 +8,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AtmController.class)
+@WebMvcTest
 class AtmControllerTest {
 
     @Autowired
@@ -23,23 +23,25 @@ class AtmControllerTest {
 
     @ParameterizedTest()
     @CsvSource({
-            "/atm/example_1_request.json, /atm/example_1_response.json",
-            "/atm/example_2_request.json, /atm/example_2_response.json"
+            "/atms/calculateOrder , /atm/example_1_request.json, /atm/example_1_response.json",
+            "/atms/calculateOrder , /atm/example_2_request.json, /atm/example_2_response.json",
+            "/onlinegame/calculate , /game/example_request.json, /game/example_response.json",
+            "/transactions/report , /transaction/example_request.json, /transaction/example_response.json"
     })
-    void calculateAtmsOrder_shouldGenerateExpectedOrder(String request, String response) throws Exception {
+    void calculateAtmsOrder_shouldGenerateExpectedOrder(String url, String request, String response) throws Exception {
 
         String requestJson = getFileAsString(request);
         String responseJson = getFileAsString(response);
 
-        this.mockMvc.perform(post("/atms/calculateOrder")
+        this.mockMvc.perform(post(url)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson))
-                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json(responseJson));
     }
 
-    private String getFileAsString(String file) throws IOException {
-        return new String(Objects.requireNonNull(this.getClass().getResourceAsStream(file)).readAllBytes());
+    private String getFileAsString(String filePath) throws IOException {
+        InputStream is = Objects.requireNonNull(this.getClass().getResourceAsStream(filePath));
+        return new String(is.readAllBytes());
     }
 }
