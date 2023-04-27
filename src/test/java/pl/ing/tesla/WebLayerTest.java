ab8d3b5be90650d85907f893365674cf7f039851
Bartosz Miller <miller.bartek@gmail.com>
2023-04-27 20:58:51 +0200

    TESLA: rename test

--- src/test/java/pl/ing/tesla/WebLayerTest.java
+++ src/test/java/pl/ing/tesla/WebLayerTest.java
@@ -1,7 +1,8 @@
 package pl.ing.tesla;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -10,25 +11,22 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
-class AtmControllerTest {
+class WebLayerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @ParameterizedTest()
-    @CsvSource({
-            "/atms/calculateOrder , /atm/example_1_request.json, /atm/example_1_response.json",
-            "/atms/calculateOrder , /atm/example_2_request.json, /atm/example_2_response.json",
-            "/onlinegame/calculate , /game/example_request.json, /game/example_response.json",
-            "/transactions/report , /transaction/example_request.json, /transaction/example_response.json"
-    })
-    void calculateAtmsOrder_shouldGenerateExpectedOrder(String url, String request, String response) throws Exception {
+    @MethodSource("provideUrlAndRequestAndResponse")
+    void apiShouldReturnExpectedResponse(String url, String request, String response) throws Exception {
 
         String requestJson = getFileAsString(request);
         String responseJson = getFileAsString(response);
@@ -36,8 +34,18 @@ class AtmControllerTest {
         this.mockMvc.perform(post(url)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json(responseJson));
+    }
+
+    public static Stream<Arguments> provideUrlAndRequestAndResponse() {
+        return Stream.of(
+                Arguments.of("/atms/calculateOrder", "/atm/example_1_request.json", "/atm/example_1_response.json"),
+                Arguments.of("/atms/calculateOrder", "/atm/example_2_request.json", "/atm/example_2_response.json"),
+                Arguments.of("/onlinegame/calculate", "/game/example_request.json", "/game/example_response.json"),
+                Arguments.of("/transactions/report", "/transaction/example_request.json", "/transaction/example_response.json")
+        );
     }
 
     private String getFileAsString(String filePath) throws IOException {
